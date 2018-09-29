a0e3cbf6cfda4dd9281efb3df93030c881a9864f
David E. Weekly <david@weekly.org>
2018-09-30 00:33:36 +0300

    update welcome views

--- Othello3/WelcomeViewController.m
+++ Othello3/WelcomeViewController.m
@@ -7,8 +7,6 @@
 //
 
 #import "WelcomeViewController.h"
-
-
 @implementation WelcomeViewController
 
 - (void)viewDidLoad
@@ -17,13 +15,11 @@
     _app =  (AppDelegate *)[[UIApplication sharedApplication] delegate];
 }
 
-
 - (IBAction)newGame:(id)sender {
     // the user wants to play a new game, take them to the opponent selection screen.
     _app.opponentViewController.modalTransitionStyle = UIModalTransitionStyleFlipHorizontal;
     [self presentViewController:_app.opponentViewController animated:YES completion:NULL];
 }
-
 
 // The user wishes to provide feedback
 - (IBAction)feedback:(id)sender {
@@ -32,17 +28,11 @@
 #endif
 }
 
-
 // Show the About view.
 - (IBAction)about:(id)sender {
     // Show the About view.
     _app.aboutViewController.modalTransitionStyle = UIModalTransitionStylePartialCurl;
     [self presentViewController:_app.aboutViewController animated:YES completion:NULL];
-}
-
-
-- (void)viewDidUnload {
-    [super viewDidUnload];
 }
 
 @end
